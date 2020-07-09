4cd2590d25e286507254a58dfd42d02d24d13536
Pablo <p.riesco@digipen.edu>
2020-07-09 14:07:56 +0200

    Fix some bugs and image rendering

--- Project2/Assets/Scripts/TutorialText.cs
+++ Project2/Assets/Scripts/TutorialText.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialText : MonoBehaviour{
     // Start is called before the first frame update
@@ -13,6 +14,7 @@ public class TutorialText : MonoBehaviour{
     public string AimText;
     public string MarkText;
     public string ShootText;
+    public string nothingText;
     public GameObject background;
     public GameObject sprite;
 
@@ -23,13 +25,15 @@ public class TutorialText : MonoBehaviour{
     // Update is called once per frame
     void Update()
     {
+        Color temp = background.GetComponent<Image>().color;
+
         switch (textToShow)
         {
             case 0:
                 GetComponent<UnityEngine.UI.Text>().text = WelcomeText;
                 sprite.SetActive(true);
-                background.SetActive(true);
-                gameObject.SetActive(true);
+
+                background.GetComponent<Image>().color = new Color(temp.r, temp.g, temp.b, 0.75f);
                 break;
             case 1:
                 GetComponent<UnityEngine.UI.Text>().text = BasicConceptText;
@@ -50,14 +54,13 @@ public class TutorialText : MonoBehaviour{
                 GetComponent<UnityEngine.UI.Text>().text = ShootText;
                 break;
             case 7:
+                GetComponent<UnityEngine.UI.Text>().text = nothingText;
                 sprite.SetActive(false);
-                background.SetActive(false);
-                gameObject.SetActive(false);
-                break;
 
+                background.GetComponent<Image>().color = new Color(temp.r, temp.g, temp.b, 0.0f);
+                break;
             default:
                 break;
-
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
