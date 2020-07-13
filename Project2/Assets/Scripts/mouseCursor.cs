a3cd68d0ddb323b9489bc86dfef1e16ac9f8f7a4
diego.sanz <diego.sanz@digipen.edu>
2020-07-13 13:45:43 +0200

    solve errors involving the mouse cursor image

--- Project2/Assets/Scripts/mouseCursor.cs
+++ Project2/Assets/Scripts/mouseCursor.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class mouseCursor : MonoBehaviour
 {
-    public RectTransform cursor;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +13,7 @@ public class mouseCursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //cursor.transform.position = cursorPos;
-        cursor.position = Input.mousePosition;
-        Debug.Log("Mouse Pos: " + Input.mousePosition);
+        transform.position = Input.mousePosition;
     }
 }
